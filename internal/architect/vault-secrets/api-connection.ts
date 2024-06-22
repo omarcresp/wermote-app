4a7a0faa5a4380cdfa6c06b00d9993c7b1dc7da8
Omar Crespo <crespomerchano@gmail.com>
2024-06-22 15:41:52 -0500

    new pipelines with staging env

--- internal/architect/vault-secrets/api-connection.ts
+++ internal/architect/vault-secrets/api-connection.ts
@@ -1,20 +1,20 @@
-import { ofetch } from "ofetch";
-import { ISecretResponse } from "./types";
-import { parseSecretsResponse } from "./map-secrets";
+import { ofetch } from 'ofetch';
+import { ISecretResponse } from './types';
+import { parseSecretsResponse } from './map-secrets';
 
 async function generateToken(): Promise<string> {
   const encodedParams = new URLSearchParams();
 
-  encodedParams.set("client_id", process.env.HCP_CLIENT_ID ?? "");
-  encodedParams.set("client_secret", process.env.HCP_CLIENT_SECRET ?? "");
-  encodedParams.set("grant_type", "client_credentials");
-  encodedParams.set("audience", "https://api.hashicorp.cloud");
+  encodedParams.set('client_id', process.env.HCP_CLIENT_ID ?? '');
+  encodedParams.set('client_secret', process.env.HCP_CLIENT_SECRET ?? '');
+  encodedParams.set('grant_type', 'client_credentials');
+  encodedParams.set('audience', 'https://api.hashicorp.cloud');
 
   const { access_token } = await ofetch(
-    "https://auth.idp.hashicorp.com/oauth2/token",
+    'https://auth.idp.hashicorp.com/oauth2/token',
     {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encodedParams,
     },
   );
@@ -23,15 +23,15 @@ async function generateToken(): Promise<string> {
 }
 
 const VAULT_PROJECTS: Record<string, string> = {
-  prod: "2d109f11-6f8e-40bf-a201-e6eebc56d083",
-  dev: "e88a564f-3833-4709-b0b5-3c4d2344ea84",
+  prod: '2d109f11-6f8e-40bf-a201-e6eebc56d083',
+  dev: 'e88a564f-3833-4709-b0b5-3c4d2344ea84',
 };
 
 export async function retrieveSecrets(appName: string, env: string) {
   const token = await generateToken();
 
-  const hashiApi = "https://api.cloud.hashicorp.com/secrets/2023-06-13";
-  const orgId = "e97c9086-24ef-4328-bb76-f6ba027cd240";
+  const hashiApi = 'https://api.cloud.hashicorp.com/secrets/2023-06-13';
+  const orgId = 'e97c9086-24ef-4328-bb76-f6ba027cd240';
   const projectId = VAULT_PROJECTS[env];
 
   const baseURL = `${hashiApi}/organizations/${orgId}/projects/${projectId}/`;
